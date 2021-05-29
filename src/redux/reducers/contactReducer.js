633637626b88db31cf536604a20f720f2e39e31a
Zebaanikanibir <zanikanibir@gmail.com>
2021-05-29 16:03:59 +0600

    done

--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -32,14 +32,3 @@ const initialState = [
   };
 
 
-//  contactReducer = (state = initialState, action) => {
-//       switch(action.type) {
-//           case "ADD_CONTACT":
-//               state=[...state,action.payload];
-//               return state;
-//           default:
-//               return state;
-//       }
-//   }
-
-//   export default contactReducer;
